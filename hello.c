94c10227184054e8bd67b2fb2520281b62e20780
Tanner Hoelzel <thoelze1@binghamton.edu>
2016-09-26 00:56:54 -0400

    Add hello world module

---
+++ hello.c
@@ -0,0 +1,25 @@
+/* Kernel Programming */
+
+#include <linux/module.h> /* Needed by all modules */
+#include <linux/kernel.h> /* Needed for KERN_INFO, KERN_ALERT */
+#include <linux/init.h> /* Needed for __init and __exit macros */
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Hoelzel");
+MODULE_DESCRIPTION("A simple Hello World module");
+
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "Hello world!\n");
+
+	/* A non 0 return means init_module failed; module can't be loaded */
+	return 0;
+}
+
+static void __exit hello_cleanup(void)
+{
+	printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
